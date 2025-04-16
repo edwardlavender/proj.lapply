88ed8d78cadd17b2adcc94c62b60e8c418d23ab8
edwardlavender <edward.lavender@eawag.ch>
2025-04-16 10:09:30 +0200

    Use dot pronoun for consistency

--- R/coffee.R
+++ R/coffee.R
@@ -1,23 +1,23 @@
 #' @title Task management
 #' @description Give your computer 'coffee breaks' during iterative activities for rest. 
-#' @param nodenames A `character` vector that defines the name(s) of the nodes on which to take coffee breaks. 
-#' @param folder A `character` that defines the folder in which coffee break times are stored. This is created, recursively, if required.
-#' @param interval A `number` that defines the minimum time interval (s) between sequential coffee breaks. 
-#' @param duration A `number` that defines the duration (s) of the coffee break.
-#' @param args `NULL`, `list()` or a named `list` of arguments used to implement [`coffee()`].
+#' @param .nodenames A `character` vector that defines the name(s) of the nodes on which to take coffee breaks. 
+#' @param .folder A `character` that defines the folder in which coffee break times are stored. This is created, recursively, if required.
+#' @param .interval A `number` that defines the minimum time interval (s) between sequential coffee breaks. 
+#' @param .duration A `number` that defines the duration (s) of the coffee break.
+#' @param .args `NULL`, `list()` or a named `list` of arguments used to implement [`coffee()`].
 #' @details 
 #' * [`coffee()`] is designed to be called in iterative applications (i.e., loops). 
-#'    * For machines (`Sys.info()["nodename"]`) in `nodenames`, the function looks up the time since the last coffee break. 
-#'    * If the time since the last break exceeds `interval`, the machine sleeps for `duration` seconds. 
-#'    * After the break, the function records the system time in a `time.qs` file written to `folder`. 
+#'    * For machines (`Sys.info()["nodename"]`) in `.nodenames`, the function looks up the time since the last coffee break. 
+#'    * If the time since the last break exceeds `.interval`, the machine sleeps for `duration` seconds. 
+#'    * After the break, the function records the system time in a `time.qs` file written to `.folder`. 
 #'
-#' * Use [`coffee_do()`] to run [`coffee()`] given `args`.
+#' * Use [`coffee_do()`] to run [`coffee()`] given `.args`.
 #'    
-#' * Use [`coffee_cleanup()`] after all iterations to `unlink(folder)`.
+#' * Use [`coffee_cleanup()`] after all iterations to `unlink(.folder)`.
 #' 
 #' @return All functions return `invisible(NULL)`. 
-#' * [`coffee()`] and [`coffee_do()`] create `folder` and `time.qs` files as side effects.
-#' * [`coffee_cleanup()`] unlinks `folder` as a side effect. 
+#' * [`coffee()`] and [`coffee_do()`] create `.folder` and `time.qs` files as side effects.
+#' * [`coffee_cleanup()`] unlinks `.folder` as a side effect. 
 #' @author Edward Lavender
 #' @name coffee
 NULL
@@ -25,21 +25,21 @@ NULL
 #' @name coffee
 #' @export
 
-coffee <- function(nodenames = Sys.info()["nodename"],
-                   folder = file.path(tempdir(), "coffee"),
-                   interval = 2 * 60 * 60,
-                   duration = 15 * 60) {
+coffee <- function(.nodenames = Sys.info()["nodename"],
+                   .folder = file.path(tempdir(), "coffee"),
+                   .interval = 2 * 60 * 60,
+                   .duration = 15 * 60) {
   
   # Take breaks on selected nodename(s) only
-  if (!(Sys.info()["nodename"] %in% nodenames)) {
+  if (!(Sys.info()["nodename"] %in% .nodenames)) {
     return(nothing())
   }
   
   # Identify time since last 'coffee' break
-  if (!dir.exists(folder)) {
-    dir.create(folder, recursive = TRUE)
+  if (!dir.exists(.folder)) {
+    dir.create(.folder, recursive = TRUE)
   }
-  time.qs <- file.path(folder, "time.qs")
+  time.qs <- file.path(.folder, "time.qs")
   if (!file.exists(time.qs)) {
     qs::qsave(Sys.time(), time.qs)
     return(nothing())
@@ -48,9 +48,9 @@ coffee <- function(nodenames = Sys.info()["nodename"],
   time_since_coffee <- difftime(Sys.time(), time, units = "secs")
   
   # Take a break if needed
-  if (time_since_coffee >= interval) {
+  if (time_since_coffee >= .interval) {
     # Sleep for `duration`
-    Sys.sleep(duration)
+    Sys.sleep(.duration)
     # Record the time of the break
     # (We'll sleep in another `duration` secs)
     qs::qsave(Sys.time(), time.qs)
@@ -63,9 +63,9 @@ coffee <- function(nodenames = Sys.info()["nodename"],
 #' @name coffee
 #' @export
 
-coffee_do <- function(args) {
-  if (!is.null(args)) {
-    do.call(coffee, args)
+coffee_do <- function(.args) {
+  if (!is.null(.args)) {
+    do.call(coffee, .args)
   }
   nothing()
 }
@@ -73,10 +73,10 @@ coffee_do <- function(args) {
 #' @name coffee
 #' @export
 
-coffee_cleanup <- function(args) {
-  if (!is.null(args)) {
-    if (rlang::has_name(args, "folder")) {
-      coffeehouse <- args$folder
+coffee_cleanup <- function(.args) {
+  if (!is.null(.args)) {
+    if (rlang::has_name(.args, "folder")) {
+      coffeehouse <- .args$folder
     } else {
       coffeehouse <- formals("coffee")$folder
     }
